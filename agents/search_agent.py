de69f9aa9b85a35ee17e3a033e5ebe74f7f39dac
tienthm <tientohoangminh@gmail.com>
2025-07-19 19:27:57 +0700

    Refactor SearchAgent class and remove unused DuckDuckGo search function

--- agents/search_agent.py
+++ agents/search_agent.py
@@ -1,48 +1,19 @@
 from agno.tools.googlesearch import GoogleSearchTools
-from agno.tools.duckduckgo import DuckDuckGoTools
 import json
+import warnings
+warnings.filterwarnings("ignore")
 
 
-# def search_google(query: str) -> None:
-#     """
-#     Use this function to get infomation from Google Search
+class SearchAgent:
+    def __init__(self, max_results: input = 3):
+        self.tool = GoogleSearchTools()
+        self.max_results = max_results
 
-#     Args:
-#         query (str): xxx
-
-#     Returns:
-#         str: xxx
-#     """
-#     tool = GoogleSearchTools()
-#     result = tool.google_search(query)
-#     result = json.loads(result)
-#     for i in result:
-#         print("------------------------------")
-#         print(i['title'])
-#         print(i['description'])
-
-#     print("------------------------------")
-
-
-def search_duckduckgo(query: str) -> str:
-    """
-    Use this function to look up infomation.
-
-    Args:
-        query (str): content
-
-    Returns:
-        str: 5 similarity result
-    """
-    tool = DuckDuckGoTools()
-    result = tool.duckduckgo_search(query)
-    result = json.loads(result)
-    temp = []
-    for i in result:
-        temp.append({"title": i['title'], "content": i["body"]})
-    return temp
-
-
-result = search_duckduckgo(
-    "Ngày 6.5, Công an thị xã Phước Long, tỉnh Bình Phước đã tạm giữ bà Lê Thị Xuyến (46 tuổi, ngụ phường Long Phước, thị xã Phước Long) để điều tra về việc bắt cóc trẻ em.")
-print(result)
+    def search(self, query: str) -> None:
+        response = self.tool.google_search(query, self.max_results)
+        response = json.loads(response)
+        result = ""
+        for i in response:
+            if i['title']:
+                result += f"Title: {i['title']} \nDescription: {i['description']}\n"
+        return result
