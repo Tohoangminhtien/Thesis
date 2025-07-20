ed64a06b7104719d3d378bf92049eeb15ae7e4ce
tienthm <tientohoangminh@gmail.com>
2025-07-20 22:44:49 +0700

    Update authenticate agent

--- agents/fact_checker.py
+++ agents/fact_checker.py
@@ -2,9 +2,12 @@ from agents.base_agent import BaseAgent
 
 
 class FactCheckerAgent(BaseAgent):
-    def __init__(self, model, instruction_prompt):
-        super().__init__(model, instruction_prompt)
+    def __init__(self, model, instruction_prompt, max_tokens=100):
+        super().__init__(model, instruction_prompt, max_tokens=max_tokens)
 
-    def check(self, **kwangs):
-        response = self.ask(**kwangs)
+    def check(self, statement: str, search_results: str):
+        user_prompt = (
+            f"Statement: {statement}\nSearch Results: {search_results}\nOutput:"
+        )
+        response = self.ask(user_prompt=user_prompt)
         return response
