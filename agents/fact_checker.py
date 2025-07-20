c7aee169487b5626c6bf9df71e11fda3b331d3cf
tienthm <tientohoangminh@gmail.com>
2025-07-20 08:31:40 +0700

    Update fact checker agent and prompt

---
+++ agents/fact_checker.py
@@ -0,0 +1,10 @@
+from agents.base_agent import BaseAgent
+
+
+class FactCheckerAgent(BaseAgent):
+    def __init__(self, model, instruction_prompt):
+        super().__init__(model, instruction_prompt)
+
+    def classify(self, **kwangs):
+        response = self.ask(**kwangs)
+        return response
