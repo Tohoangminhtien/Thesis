0c7658447768b2368381cb4fe01b3758ec2af563
tienthm <tientohoangminh@gmail.com>
2025-07-20 22:13:52 +0700

    Update classify agent

--- agents/classify_agent.py
+++ agents/classify_agent.py
@@ -2,9 +2,10 @@ from agents.base_agent import BaseAgent
 
 
 class ClassifyAgent(BaseAgent):
-    def __init__(self, model, instruction_prompt):
-        super().__init__(model, instruction_prompt)
+    def __init__(self, model, instruction_prompt, max_tokens=100):
+        super().__init__(model, instruction_prompt, max_tokens)
 
-    def classify(self, **kwangs):
-        response = self.ask(**kwangs)
+    def classify(self, statement: str):
+        user_prompt = f"Statement: {statement}\nOutput:"
+        response = self.ask(user_prompt=user_prompt)
         return response
