f01dab496d2a0a7336034c272aa055d000651fe7
tienthm <tientohoangminh@gmail.com>
2025-07-23 00:54:34 +0700

    Code run fact checker

--- work_flow.py
+++ work_flow.py
@@ -35,46 +35,25 @@ authentication_agent = AuthenticateAgent(
 )
 
 # Load the dataset
-df = pd.read_csv("data/test.csv")
-new_df = pd.DataFrame(
-    columns=[
-        "statement",
-        "fact_confidence",
-        "classify_confidence",
-        "authentication_confidence",
-        "label_prediction",
-        "label",
-    ]
-)
-NUM_TEST = 10
-for i in range(NUM_TEST):
+df = pd.read_csv("./results/classify_agent_results.csv")
+idx_from = 101
+new_df = pd.DataFrame(columns=["statement", "fact_confidence", "label"])
+
+
+for i in range(idx_from, len(df)):
     statement = df["statement"][i]
-    subject = df["subject"][i]
-    job_title = df["job_title"][i]
-    speaker = df["speaker"][i]
-    party_affiliation = df["party_affiliation"][i]
-    state_info = df["state_info"][i]
-    context = df["context"][i]
     label = df["label"][i]
-    metadata = {
-        "Subject": subject,
-        "Speaker": speaker,
-        "Job title": job_title,
-        "State info": state_info,
-        "Party_affiliation": party_affiliation,
-        "Context": context,
-    }
 
     print(f"Statement {statement}")
 
     # Legality Agent
-    legal_response = legal_agent.check(statement=statement)
-    if not legal_response:
-        continue
+    # legal_response = legal_agent.check(statement=statement)
+    # if not legal_response:
+    #     continue
 
     # Classify Agent
-    classify_response = classify_agent.classify(statement=statement)
-    classify_response = json.loads(classify_response)
+    # classify_response = classify_agent.classify(statement=statement)
+    # classify_response = json.loads(classify_response)
 
     # Search Agent
     search_response = search_agent.search(query=statement)
@@ -86,17 +65,14 @@ for i in range(NUM_TEST):
     fact_response = json.loads(fact_response)
 
     # Authenticate Agent
-    auth_response = authentication_agent.check(metadata=metadata)
-    auth_response = json.loads(auth_response)
+    # auth_response = authentication_agent.check(metadata=metadata)
+    # auth_response = json.loads(auth_response)
 
     new_row = pd.DataFrame(
         [
             {
                 "statement": statement,
                 "fact_confidence": fact_response["confidence"],
-                "classify_confidence": classify_response["confidence"],
-                "authentication_confidence": auth_response["confidence"],
-                "label_prediction": classify_response["label"],
                 "label": label,
             }
         ]
@@ -104,4 +80,4 @@ for i in range(NUM_TEST):
 
     new_df = pd.concat([new_df, new_row], ignore_index=True)
     # Save the results to a new CSV file
-    new_df.to_csv("data/output.csv", index=False)
+    new_df.to_csv("./results/fact_checker_results_2.csv", index=False)
